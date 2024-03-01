fe7a3651d6368d58d0ab1b557a8d9ac8ddff1b07
Pie <120573783+FullMetalPie@users.noreply.github.com>
2024-03-01 17:55:44 +0100

    Add files via upload

---
+++ Scenes/Levels/Livello_1.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Transporter1_body_entered(body):
+	
+	
+	
+	if $Player.position.x > 0:
+		$Player.speed += 100
+	elif $Player.position.x < 0:
+		$Player.speed -= 100
+		
