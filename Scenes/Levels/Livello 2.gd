6d6622912c92864099cfe2b8cf88c72eea71a341
Filippo Catellani <74590518+officialBurtaxz@users.noreply.github.com>
2024-03-04 16:53:45 +0100

    Add files via upload

--- Scenes/Levels/Livello 2.gd
+++ Scenes/Levels/Livello 2.gd
@@ -11,7 +11,6 @@ func _ready():
 	extinguisher_area = $Fire_Estinguisher_Area
 	extinguisher_area.connect("is_entered_extintor", self, "_on_extinguisher_is_entered")
 	time_start = OS.get_unix_time()
-	
 	$Path2D_Fire.firespeed = 0
 	
 func _on_extinguisher_is_entered(): #vittoria
@@ -21,15 +20,16 @@ func _on_extinguisher_is_entered(): #vittoria
 	$CanvasLayer_Win/Popup_Win/Label_Win.text = ("\nHAI VINTO!\nTempo impiegato per completare il livello: " + str(time_win) + " secondi")
 	$Player.speed = 0
 	$Path2D_Fire.firespeed = 0
+	Global.is_Extinguisher_collected = true
 	
-func _process(delta):
+func _process(_delta):
 	time_now = OS.get_unix_time()
 	time_elapsed = time_now - time_start
 	$CanvasLayer/Label_FPS.text = str(Engine.get_frames_per_second()) + " FPS"
 	if $Player.velocity.x != 0:
-		$Path2D_Fire.firespeed = 215
+		$Path2D_Fire.firespeed = 225
 	
-func _on_Area2D_body_entered(body): # morte per caduta
+func _on_Area2D_body_entered(_body): # morte per caduta
 	paused = true
 	$CanvasLayer_Dead/Popup_Dead.show()
 	$Player.speed = 0
@@ -55,14 +55,14 @@ func _on_Button_Win_Esci_pressed():
 	get_tree().paused = false
 
 
-func _on_Flame_body_entered(body): # entra nell'area degli ostacoli piccoli (fuoco)
+func _on_Flame_body_entered(_body): # entra nell'area degli ostacoli piccoli (fuoco)
 	paused = true
 	$CanvasLayer_Dead/Popup_Dead.show()
 	$Player.speed = 0
 	$Path2D_Fire.firespeed = 0
 	
 
-func _on_Fire_body_entered(body): # entra nell'area del fuoco mobile
+func _on_Fire_body_entered(_body): # entra nell'area del fuoco mobile
 	paused = true
 	$CanvasLayer_Dead/Popup_Dead.show()
 	$Player.speed = 0
