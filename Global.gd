5ee38826518cb241b1021beb98ba24637846df61
Filippo Catellani <74590518+officialBurtaxz@users.noreply.github.com>
2024-03-04 16:59:17 +0100

    Add files via upload

---
+++ Global.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var is_Extinguisher_collected = false
+var is_Helmet_collected = false
+var is_Mask_collected = false
+var is_Face_Mask_collected = false
+var is_Reflective_Suit_collected = false
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
