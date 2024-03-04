e7ad5815bed40e1b95e6115b14755a05de2154fd
Pie <120573783+FullMetalPie@users.noreply.github.com>
2024-03-04 17:02:53 +0100

    Updated Lvl 1

--- Scenes/Levels/Livello_1.gd
+++ Scenes/Levels/Livello_1.gd
@@ -1,28 +1,27 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var is_in1 = false
+var is_in2 = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass # Replace with function body.\
+
+func _process(delta):
+	if is_in1:
+		$Player.position.x += 2
+	elif is_in2:
+		$Player.position.x -= 2
 
 
+func _on_Transporter_right_body_entered(body):
+	is_in1 = true
 
+func _on_Transporter_right_body_exited(body):
+	is_in1 = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Transporter_left_body_entered(body):
+	is_in2 = true
 
-func _on_Transporter1_body_entered(body):
-	
-	
-	
-	if $Player.position.x > 0:
-		$Player.speed += 100
-	elif $Player.position.x < 0:
-		$Player.speed -= 100
-		
+func _on_Transporter_left_body_exited(body):
+	is_in2 = false
