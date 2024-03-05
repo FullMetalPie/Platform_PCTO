ef2db60b7bac4acd0fd3e5597aedca5783c24280
Filippo Catellani <74590518+officialBurtaxz@users.noreply.github.com>
2024-03-05 15:11:46 +0100

    Add files via upload

--- Scenes/Levels/General_Path.gd
+++ Scenes/Levels/General_Path.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var helmet_Area : Area2D
 var is_in1 = false #TRASPORTATORE DX
 var is_in2 = false #TRASPORTATORE SX
 var inc_1_to_3 = 0
@@ -7,11 +8,18 @@ var inc_4 = 0
 var inc_5_and_10 = 194
 var inc_6_and_9 = 0
 var inc_7_and_8 = 60
+var time_start = 0
+var time_now = 0
+var time_win = 0
+export var time_elapsed = 0
 
 var speedBase = 400
 var speed4 = 400
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	helmet_Area = $Helmet_Area
+	helmet_Area.connect("is_entered_Helmet", self, "_on_Helmet_is_entered")
+	time_start = OS.get_unix_time()
 	pass # Replace with function body.\
 	
 func _on_Dead_Fall_Zone_body_entered(body):
@@ -33,6 +41,9 @@ func _on_Button_Dead_Ripirova_pressed():
 
 #Trasportatori Livello 1
 func _process(delta):
+	time_now = OS.get_unix_time()
+	time_elapsed = time_now - time_start
+	
 	if is_in1:
 		$Player.position.x += 2
 	elif is_in2:
@@ -46,7 +57,7 @@ func _process(delta):
 	$Path2D_Scatola2/PathFollow2D_Scatola2.offset = inc_1_to_3
 	$Path2D_Scatola3/PathFollow2D_Scatola3.offset = inc_1_to_3
 	
-	if $Path2D_Scatola4/PathFollow2D_Scatola4.position.x >= 1510 and $Path2D_Scatola4/PathFollow2D_Scatola4.position.x <= 1610:
+	if $Path2D_Scatola4/PathFollow2D_Scatola4.position.x >= 1530 and $Path2D_Scatola4/PathFollow2D_Scatola4.position.x <= 1610:
 		speed4 = 100
 	else:
 		speed4 = 400
@@ -62,7 +73,7 @@ func _process(delta):
 	$Path2D_Scatola6/PathFollow2D_Scatola6.offset = inc_6_and_9
 	$Path2D_Scatola9/PathFollow2D_Scatola9.offset = inc_6_and_9
 	
-	inc_7_and_8 += delta * 500
+	inc_7_and_8 += delta * speedBase
 	$Path2D_Scatola7/PathFollow2D_Scatola7.offset = inc_7_and_8
 	$Path2D_Scatola8/PathFollow2D_Scatola8.offset = inc_7_and_8
 	
@@ -84,3 +95,71 @@ func _on_Transporter_left_body_exited(body):
 	print("Uscito sx")
 	
 
+
+func _on_Helmet_is_entered():
+	if time_win == 0:
+		time_win = time_elapsed
+	$CanvasLayer_Win/Popup_Win.show()
+	$CanvasLayer_Win/Popup_Win/Label_Win.text = ("\nHAI VINTO!\nTempo impiegato per completare il livello: " + str(time_win) + " secondi")
+	$Player.speed = 0
+	Global.is_Helmet_collected = true
+
+
+func _on_Button_Win_Esci_pressed():
+	get_tree().change_scene("res://Scenes/Menu/Main.tscn")
+
+
+func _on_Button_Win_Coll_pressed():
+	get_tree().change_scene("res://Scenes/Menu/HUD_COLLECT.tscn")
+
+
+
+
+func _on_Box_Area2D_Scatola1_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
+
+
+
+func _on_Box_Area2D_Scatola2_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
+
+
+func _on_Box_Area2D_Scatola3_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
+
+
+func _on_Box_Area2D_Scatola4_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
+
+func _on_Box_Area2D_Scatola5_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
+
+
+func _on_Box_Area2D_Scatola6_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
+
+
+func _on_Box_Area2D_Scatola7_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
+
+
+func _on_Box_Area2D_Scatola8_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
+
+
+func _on_Box_Area2D_Scatola9_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
+
+
+func _on_Box_Area2D_Scatola10_body_entered(body):
+	$CanvasLayer_Dead/Popup_Dead.show()
+	$Player.speed = 0
