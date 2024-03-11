6674ee827889075e9329723df155c12f9b8679de
Filippo Catellani <74590518+officialBurtaxz@users.noreply.github.com>
2024-03-11 09:08:48 +0100

    Add files via upload

--- Global.gd
+++ Global.gd
@@ -3,8 +3,8 @@ extends Node
 var is_Extinguisher_collected = false
 var is_Helmet_collected = false
 var is_Mask_collected = false
-var is_Face_Mask_collected = false
 var is_Reflective_Suit_collected = false
+var is_Harness_collected = false
 
 var show_fps = false
 var volume = 0
