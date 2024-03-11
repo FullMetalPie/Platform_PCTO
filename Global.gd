a6505ccb07f29e40b83d6bb82eee9d0848ae7a23
Filippo Catellani <74590518+officialBurtaxz@users.noreply.github.com>
2024-03-11 12:43:36 +0100

    Add files via upload

--- Global.gd
+++ Global.gd
@@ -1,23 +1,20 @@
 extends Node
-
+#--------------VARIABILI COLLEZIONABILI-------------------
 var is_Extinguisher_collected = false
 var is_Helmet_collected = false
 var is_Mask_collected = false
 var is_Reflective_Suit_collected = false
 var is_Harness_collected = false
-
+#------------------VARIABILI LIVELLI-----------------------
+var is_in_what_lvl = 0
+#------------------VARIABILI GENERALI-----------------------
 var show_fps = false
 var volume = 0
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+		
