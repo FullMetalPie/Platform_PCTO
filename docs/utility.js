f029cd6b7de79b05b81bc42c961e9e78a1e3aae9
FullMetalPie <andrea.pietri06@gmail.com>
2024-03-12 16:12:41 +0100

    Updated website files

--- docs/utility.js
+++ docs/utility.js
@@ -3,15 +3,18 @@ var icon = document.getElementById("icon")
 
 sideNav.style.left = "-250px";
 icon.src="Media/menu_FILL0_wght400_GRAD0_opsz24.svg";
+icon.style.backgroundColor = "#262626";
 
 icon.onclick = function(){
     if(sideNav.style.left == "-250px"){
         sideNav.style.left = "0";
         icon.src="Media/close_FILL0_wght400_GRAD0_opsz24.svg";
+        icon.style.backgroundColor = "#EEEEEE";
     }
     else{
         sideNav.style.left = "-250px";
         icon.src="Media/menu_FILL0_wght400_GRAD0_opsz24.svg";
+        icon.style.backgroundColor = "#262626";
     }
 }
 
